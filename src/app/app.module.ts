67d0ef58220c335a9d0489f1eebbe805d0d1cad3
Dupeng <dupeng0811@qq.com>
2020-04-22 16:38:35 +0800

    添加购物车

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import {RouterModule} from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -24,14 +25,16 @@ import {RouterModule} from '@angular/router';
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
-      {path: 'products/:productId', component: ProductDetailsComponent}
+      {path: 'products/:productId', component: ProductDetailsComponent},
+      {path: 'cart', component: CartComponent}
     ])
   ],
   providers: [StorageserviceService],
